c2cdd7cfaeed4e6c4b76a64f3b12d09dee1600cc
Praditia Nur <pnplusplus@gmail.com>
2024-01-22 03:46:15 +0000

    Set BUILD_NUMBER same as LINEAGE_BUILD_DATE * Just cosmetic reason for me, better use unix epoch time idea by kdrag0n
    Change-Id: If18b656c86da785c02308f4aec3f66c15335e532

--- core/version_util.mk
+++ core/version_util.mk
@@ -239,7 +239,7 @@ ifndef BUILD_NUMBER
   # If no BUILD_NUMBER is set, create a useful "I am an engineering build
   # from this date/time" value.  Make it start with a non-digit so that
   # anyone trying to parse it as an integer will probably get "0".
-  BUILD_NUMBER := $(BUILD_DATETIME)
+  BUILD_NUMBER := $(shell date -u +%Y%m%d)
   HAS_BUILD_NUMBER := true
 endif
 .KATI_READONLY := BUILD_NUMBER HAS_BUILD_NUMBER
